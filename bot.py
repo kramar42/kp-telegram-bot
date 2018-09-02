9b95261a4c1e5934f3b1eb418c0d35d32703a183
Alexey Kramarenko <kramar42@gmail.com>
2018-09-03 00:22:55 +0200

    random reversed sasai

--- bot.py
+++ bot.py
@@ -167,7 +167,7 @@ def echo(bot, update, job_queue, chat_data):
 И снова третье сентября.
 
 https://youtu.be/PQ02jz-UgeA''')
-        job_queue.run_once(september_3rd_timeout, WAIT_AMOUNT, context=chat_data)
+        job_queue.run_once(september_3rd_timeout, WAIT_AMOUNT / 2, context=chat_data)
 
 
 def pidors_reminder_callback(bot, job):
@@ -240,8 +240,12 @@ def say(bot, update, args):
 
 def sasai(bot, update, args):
     who = ' '.join(args)
-    bot.send_message(chat_id=update.message.chat_id, text='%s наклоняется и делает нежный сасай %s' %
-            (who, update.message.from_user.name))
+    if random.randint(0, 1) == 0:
+        bot.send_message(chat_id=update.message.chat_id, text='%s наклоняется и делает нежный сасай %s' %
+                (who, update.message.from_user.name))
+    else:
+        bot.send_message(chat_id=update.message.chat_id, text='%s делает первоклассный сасай %s' %
+                (update.message.from_user.name, who))
 
 
 def error(bot, update, error):
