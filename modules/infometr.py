0f8c1f21b47bb896d45798c9f7a66cbba770c4be
Eugene Luppol <eluppol@12dmp.com>
2019-02-12 21:28:26 -0800

    initial implementation of infometr

---
+++ modules/infometr.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import hashlib
+from telegram.ext import CommandHandler
+
+DIGITS = 3
+
+
+def check_info(text):
+    return int(hashlib.sha512(text.encode('utf8')).hexdigest()[0:DIGITS], 16) % 101
+
+
+def infometr(bot, update, args):
+    if len(args) == 0:
+        update.message.reply_text('/infa <text>')
+        return
+
+    text = ' '.join(args)
+    update.message.reply_text('Инфа %d%%' % check_info(text))
+
+
+def start():
+    return [CommandHandler('infa', infometr, pass_args=True)]
