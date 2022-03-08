b4c841f933e4c6c7321b240f759b5f049a4f0683
Alexey Kramarenko <kramar42@gmail.com>
2022-03-09 00:03:01 +0100

    new module

---
+++ modules/moskaliki.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+from datetime import datetime
+
+from telegram.ext import CommandHandler
+
+war_start = datetime.fromisoformat('2022-02-24 04:00:00')
+
+
+def moskaliki(update, context):
+    seconds = (datetime.now() - war_start).total_seconds()
+    number = 0.694 * seconds
+    update.effective_chat.send_message(f'пройшло {number:.1f} москаликів з початку наступу')
+
+
+handlers = [CommandHandler('moskaliki', moskaliki)]
