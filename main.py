31dacbf42964393faa889e65a8ebe3ac57114471
Andrii Buriachevskyi <ua.over@gmail.com>
2023-04-17 00:46:49 +0200

    update project structure

--- main.py
+++ main.py
@@ -1,36 +1,35 @@
+import argparse
 import logging
 import os
 import sys
 
-from telegram.ext import ApplicationBuilder
-
-import modules
-
-
-async def error(update, context):
-    logging.getLogger().error("exception while handling an update:", exc_info=context.error)
+from kpbot import create_app
 
 
 def main():
-    logging.basicConfig(level=logging.INFO,
-                        format='[%(asctime)s][%(module)s][%(levelname)s] %(message)s',
-                        datefmt='%Y-%m-%d %H:%M:%S')
-    log = logging.getLogger()
+    logging.basicConfig(
+        level=logging.INFO,
+        format="[%(asctime)s][%(name)s][%(levelname)s] %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
 
-    bot_token = os.environ['BOT_TOKEN']
-    if not bot_token:
-        log.fatal('empty BOT_TOKEN')
+    try:
+        parser = argparse.ArgumentParser()
+        parser.add_argument(
+            "-t",
+            "--token",
+            required="BOT_TOKEN" not in os.environ,
+            default=os.environ.get("BOT_TOKEN"),
+            help="Telegram Bot token",
+        )
+        args = parser.parse_args()
+
+        application = create_app(args.token)
+        application.run_polling()
+    except Exception as e:
+        logging.getLogger().critical(e)
         sys.exit(1)
 
-    application = ApplicationBuilder().token(bot_token).build()
-    application.add_error_handler(error)
 
-    handlers = modules.get_handlers()
-    application.add_handlers(handlers)
-    log.info('registered all handlers')
-
-    application.run_polling()
-
-
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
