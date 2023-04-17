b26743359e443d1021d253a1d57026fc3c21d0b9
Andrii Buriachevskyi <ua.over@gmail.com>
2023-04-18 00:17:39 +0200

    format modules init

--- kpbot/modules/__init__.py
+++ kpbot/modules/__init__.py
@@ -1,12 +1,6 @@
-from . import bomb_word
-from . import chat
-from . import infometr
-from . import sasai
-from . import say
-from . import timer
-from . import moskaliki
+from . import bomb_word, chat, infometr, moskaliki, sasai, say, timer
 
 
 def get_handlers():
     modules = [bomb_word, chat, infometr, sasai, say, timer, moskaliki]
-    return [handler for m in modules for handler in getattr(m, 'handlers', [])]
+    return [handler for m in modules for handler in getattr(m, "handlers", [])]
