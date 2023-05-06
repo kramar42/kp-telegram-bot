2caa362cca39739e45120658601d399f172ccb0d
Andrii Buriachevskyi <ua.over@gmail.com>
2023-05-07 01:52:02 +0200

    use alias in sasai module

--- kpbot/modules/sasai.py
+++ kpbot/modules/sasai.py
@@ -3,16 +3,20 @@ import random
 from telegram.ext import CommandHandler
 
 from .. import reply
+from ..alias import get_alias
 
 
 async def sasai(update, context):
-    who = ' '.join(context.args)
+    message = update.effective_message
+    author = get_alias(message.chat_id, message.from_user.id) or message.from_user.name[1:]
+    target = " ".join(context.args)
+
     if random.randint(0, 1) == 0:
-        response = update.effective_chat.send_message(f'{who} нахиляється і робить ніжний сасай {update.effective_message.from_user.name}')
+        text = f"{target} нахиляється і робить ніжний сасай {author}"
     else:
-        response = update.effective_chat.send_message(f'{update.effective_message.from_user.name} робить бездоганний сасай {who}')
+        text = f"{author} робить бездоганний сасай {target}"
 
-    await reply(response)
+    await reply(update.effective_chat.send_message(text))
 
 
-handlers = [CommandHandler('sasai', sasai)]
+handlers = [CommandHandler("sasai", sasai)]
