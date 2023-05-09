2faa3fb444d2830fa3177633fbb681cfac6e6a02
Oleksii Kramarenko <kramar42@gmail.com>
2023-05-09 16:49:57 +0200

    refactor process_message into select_keys; move more functions to DatabaseClient

--- kpbot/modules/__init__.py
+++ kpbot/modules/__init__.py
@@ -1,6 +1,6 @@
-from . import bomb_word, chat, infometr, moskaliki, sasai, say, timer
+from . import bomb_word, chat, infometr, sasai, say, timer
 
 
 def get_handlers():
-    modules = [bomb_word, chat, infometr, sasai, say, timer, moskaliki]
+    modules = [bomb_word, chat, infometr, sasai, say, timer]
     return [handler for m in modules for handler in getattr(m, "handlers", [])]
--- kpbot/modules/sasai.py
+++ kpbot/modules/sasai.py
@@ -2,17 +2,17 @@ import random
 
 from telegram.ext import CommandHandler
 
-from ..db import send_and_archive
+from .. import reply
 
 
 async def sasai(update, context):
     who = ' '.join(context.args)
     if random.randint(0, 1) == 0:
-        reply = update.effective_chat.send_message(f'{who} нахиляється і робить ніжний сасай {update.effective_message.from_user.name}')
+        response = update.effective_chat.send_message(f'{who} нахиляється і робить ніжний сасай {update.effective_message.from_user.name}')
     else:
-        reply = update.effective_chat.send_message(f'{update.effective_message.from_user.name} робить бездоганний сасай {who}')
+        response = update.effective_chat.send_message(f'{update.effective_message.from_user.name} робить бездоганний сасай {who}')
 
-    await send_and_archive(reply)
+    await reply(response)
 
 
 handlers = [CommandHandler('sasai', sasai)]
