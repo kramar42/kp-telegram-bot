6c89452def32cf0a25d5d076ba67e966b4571258
Andrii Buriachevskyi <ua.over@gmail.com>
2023-05-14 02:31:10 +0200

    add admin module

--- kpbot/modules/__init__.py
+++ kpbot/modules/__init__.py
@@ -1,6 +1,6 @@
-from . import ai, bomb_word, chat, infometr, sasai, say, timer
+from . import admin, ai, bomb_word, chat, infometr, sasai, say, timer
 
 
 def get_handlers():
-    modules = [ai, bomb_word, chat, infometr, sasai, say, timer]
+    modules = [admin, ai, bomb_word, chat, infometr, sasai, say, timer]
     return [handler for m in modules for handler in getattr(m, "handlers", [])]
